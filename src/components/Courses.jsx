9fa15a663a8531875b196b64dc71fef2503eed4d
umayr-dev <abuqodir0107@gmail.com>
2025-04-04 23:38:41 +0500

    commit

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -7,7 +7,9 @@ import Modal from "./Modal";
 function Courses() {
   const { t } = useTranslation();
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const formatPrice = (price) => {
+    return price.toLocaleString("ru-RU"); // Narxni "3 999 000" kabi formatlash
+  };
   return (
     <>
       <div className="container">
@@ -15,7 +17,7 @@ function Courses() {
           {["course1", "course2", "course3"].map((course, index) => (
             <div className="course-card" key={index}>
               <h1>{t(course)}</h1>
-              <span className="cost">{[3_999_000, 6_499_000, 12_999_000][index]} {t("cost")}</span>
+              <span className="cost">{formatPrice([3999000, 6499000, 12999000][index])} {t("cost")}</span>
               <span>{t(`tarif${index + 1}`)}</span>
               <button onClick={() => setIsModalOpen(true)}>{t("btn")}</button>
             </div>
